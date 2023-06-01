ec2b5f7c6561805de39fdc91be01f73f19e449c6
supersakana <zacwilliamson@icloud.com>
2023-05-31 19:13:34 -0600

    moved all App.vue methods into store

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <GameScore :current="current_score" :best="best_score" />
         <div class="my-10 h-[70vh] grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
           <div :key="index" v-for="(char, index) in chars">
-            <GameCard @play-round="playRound" :char="char" />
+            <GameCard @play-round="storeGame.playRound" :char="char" />
           </div>
         </div>
         <span>{{ storeGame.name }}</span>
@@ -37,26 +37,7 @@ export default {
     }
   },
   mounted() {
-    this.shuffle()
-  },
-  methods: {
-    shuffle(){
-      this.chars = this.chars.sort(() => Math.random() - 0.5);
-    },
-    playRound(char){
-      this.shuffle()
-      if(this.storeGame.memory.includes(char)){
-        this.storeGame.memory = []
-        this.storeGame.current_score = 0
-      } else {
-        this.storeGame.memory = [...this.storeGame.memory, char]
-        this.storeGame.current_score = this.storeGame.current_score + 1
-        if(this.storeGame.current_score >  this.storeGame.best_score) this.storeGame.best_score = this.storeGame.current_score
-      }
-    }
+    this.storeGame.shuffle()
   }
 }
 </script>
-
-<style>
-</style>
--- src/store/game.js
+++ src/store/game.js
@@ -8,8 +8,29 @@ export const useGameStore = defineStore("id", {
         chars: ['火', '水', '木', '風','日','月',
                 '火', '水', '木', '風','日','月'],
     }),
-    getters: {
-    },
     actions: {
+        correctGuess(char){
+            this.memory = [...this.memory, char]
+            this.current_score++
+
+            if(this.current_score > this.best_score){
+                this.best_score = this.current_score
+            }
+        },
+        wrongGuess() {
+            this.memory = []
+            this.current_score = 0
+        },
+        shuffle(){
+            this.chars = this.chars.sort(() => Math.random() - 0.5)
+        },
+        playRound(char) {
+            this.shuffle()
+            if(this.memory.includes(char)){
+                this.wrongGuess()
+            } else {
+                this.correctGuess(char)
+            }
+        }
     }
 })
